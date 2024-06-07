58744d93702e9c8ab04d7808aa29ca28495b8b5f
AzhraMeisa09 <azhrameisa@gmail.com>
2024-06-07 16:00:42 +0700

    edit & hapus berita by adminfti

--- middleware/uploadgambar.js
+++ middleware/uploadgambar.js
@@ -1,33 +1,42 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 // Konfigurasi penyimpanan multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'src/uploads/'); // Menyimpan gambar di folder 'uploads'
+    const userId = req.userId; // Replace this with how you get the user's ID
+    const dir = `src/uploads/${userId}`;
+
+    // Create directory if it doesn't exist
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+
+    cb(null, dir);
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); // Menambahkan timestamp pada nama file
-  }
+    cb(null, "foto.jpg");
+  },
 });
 
 // Middleware untuk memeriksa tipe file gambar
 const fileFilter = (req, file, cb) => {
-  const filetypes = /jpeg|jpg|png|gif/;
+  const filetypes = /jpeg|jpg|png|pdf/;
   const mimetype = filetypes.test(file.mimetype);
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
   if (mimetype && extname) {
     return cb(null, true);
   } else {
-    cb('Error: Images Only!');
+    cb("Error: Invalid file type!");
   }
 };
 
 const upload = multer({
   storage: storage,
   limits: { fileSize: 1024 * 1024 * 5 }, // Maksimal ukuran file 5MB
-  fileFilter: fileFilter
+  fileFilter: fileFilter,
 });
 
 module.exports = upload;
--- src/index.js
+++ src/index.js
@@ -6,26 +6,27 @@ const cookieParser = require("cookie-parser");
 const socketio = require("socket.io");
 const chat = require("../controller/socket");
 const app = express();
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const port = 3000;
 const server = http.createServer(app);
 const io = socketio(server);
-const upload = require('../middleware/uploadgambar'); 
+const upload = require("../middleware/uploadgambar");
 
 app.set("view engine", "ejs");
 
-const uploadDir = path.join(__dirname, 'uploads');
+const uploadDir = path.join(__dirname, "uploads");
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
 
+app.use("/uploads", express.static(path.join(__dirname, "src/uploads")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/assets", express.static("public"));
 app.use(cookieParser());
 app.use(express.json());
-app.use(router);
+app.use("/", router);
 
 chat(io);
 
